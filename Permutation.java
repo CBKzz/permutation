e90e5e94a7b3e2a9645aa5baa4a97f27f8317db7
CBKzz <baokuncheng@gmail.com>
2016-06-09 15:51:23 -0400

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -23,3 +23,46 @@ public class Solution {
         }
     }
 }
+
+
+//Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+For example,
+[1,1,2] have the following unique permutations:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+
+
+
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result=new ArrayList<>();
+        helper(result, nums, 0, new ArrayList<>());
+        return result;
+    }
+    public void helper(List<List<Integer>> result, int[] nums, int index, List<Integer> list){
+        if(index==nums.length){
+            result.add(new ArrayList<>(list));
+            return ;
+        }
+        Set<Integer> set=new HashSet<>();
+        for(int i=index;i<nums.length;i++){
+            if(!set.contains(nums[i])){
+            list.add(nums[i]);
+            set.add(nums[i]);
+            swap(nums,i, index);
+            helper(result,nums, index+1, list);
+            list.remove(list.size()-1);
+            swap(nums,i, index);
+            }
+        }
+    }
+    public void swap(int [] nums, int i, int j){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+}
